78cdb2c324214bdd18dc0fa68ab02380dcc3fb89
wiiaam <wiiam24@gmail.com>
2017-06-21 17:41:27 +1200

    invites

--- src/main/scala/modules/Invite.scala
+++ src/main/scala/modules/Invite.scala
@@ -15,11 +15,11 @@ class Invite extends BotModule{
       if(m.sender.isAdmin){ //cooldown <= System.currentTimeMillis()/1000
         r.join(m.trailing)
         r.say(m.trailing, s"${m.sender.nickname} has invited me here. To see my commands, use .help")
-        cooldown = System.currentTimeMillis()/1000 + 30
+        //cooldown = System.currentTimeMillis()/1000 + 30
       }
       else{
-        val timeleft = Math.ceil(cooldown - (System.currentTimeMillis()/1000)).asInstanceOf[Int]
-        r.notice(m.sender.nickname, s"I am currently on invite cooldown. Please wait another $timeleft seconds and invite me again")
+        //val timeleft = Math.ceil(cooldown - (System.currentTimeMillis()/1000)).asInstanceOf[Int]
+        r.notice(m.sender.nickname, "Invites are currently disabled")//s"I am currently on invite cooldown. Please wait another $timeleft seconds and invite me again")
       }
     }
   }
